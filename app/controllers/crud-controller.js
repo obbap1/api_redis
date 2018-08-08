3379b9ae3a230275c19c0aeeb18be0c2942a118f
obba P <paschalobba@yahoo.com>
2018-08-08 14:24:18 +0100

    db fix

--- app/controllers/crud-controller.js
+++ app/controllers/crud-controller.js
@@ -25,7 +25,7 @@ db.connect()
 
 module.exports = {
     getData : (req,res)=>{
-        db.any('SELECT * FROM user')
+        db.any('SELECT * FROM data')
           .then(data=>{
               console.log('get all the data',data);
               return res.status(200).json(data);
@@ -36,7 +36,7 @@ module.exports = {
     },
     getSingle : (req,res)=>{
         const userID = parseInt(req.params.id);
-        db.one('SELECT * FROM user WHERE id = $1',userID)
+        db.one('SELECT * FROM data WHERE id = $1',userID)
             .then(data=>{
                 console.log('single data',data);
                 return res.status(200).json(data);
@@ -46,9 +46,9 @@ module.exports = {
             })
     },
     postData: (req,res) =>{
+        console.log(req.body.firstName,req.body.age,req.body.address,req.body.sex,'name');
         req.body.age = parseInt(req.body.age);
-        db.none(`insert into user(name,age,address,sex)' + 
-                'values(${name},${age},${address},${sex})`,req.body)
+        db.none("INSERT INTO data (name,age,address,sex)" + "VALUES($1,$2,$3,$4)",[req.body.firstName,req.body.age,req.body.address,req.body.sex]) 
             .then(success=>{
                 res.status(200).send('Success!');
             })
@@ -57,9 +57,25 @@ module.exports = {
             })
     },
     updateData: (req,res) =>{
-        db.none('update user')
+        db.none('update data set name=$1, age=$2, address=$3, sex=$4 where id=$5',
+        [req.body.name, parseInt(req.body.age), req.body.address, req.body.sex,parseInt(req.params.id)])
+        .then(response=>{
+            console.log('response update',response);
+            return res.status(200).json('This users data has been updated successfully');
+        })
+        .catch(err=>{
+            return res.status(401).json('Invalid Input!');
+        });
     },
     deleteData: (req,res) =>{
-
+        let userID = parseInt(req.params.id);
+        db.result('delete from data where id=$1',userID)
+        .then(result=>{
+            console.log('This user has been deleted!',result);
+            res.status(200).json('success!!!');
+        })
+        .catch(error=>{
+            res.status(401).json('error!');
+        });
     }
 }
